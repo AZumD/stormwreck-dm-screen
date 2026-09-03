5f42d614ef98050dd3610806ae8ff95f8a4d71ae
AZumD <anthon.zumendorf@gmail.com>
2026-09-04 01:40:53 +0200

    complete Swedish character creator chrome

---
+++ js/i18n/sv-creator.js
@@ -0,0 +1,22 @@
+(function () {
+  "use strict";
+
+  window.I18N_SV = window.I18N_SV || {};
+  window.I18N_SV.site = window.I18N_SV.site || {};
+  window.I18N_SV.site.creator = Object.assign({}, window.I18N_SV.site.creator || {}, {
+    characterName: "Rollpersonens namn",
+    steps: {
+      start: "Kom igång",
+      class: "Klass",
+      species: "Släkte",
+      background: "Bakgrund",
+      abilities: "Grundegenskaper",
+      feats: "Feats",
+      spells: "Besvärjelser",
+      equipment: "Utrustning",
+      identity: "Identitet",
+      about: "Om rollpersonen",
+      review: "Granska"
+    }
+  });
+})();
